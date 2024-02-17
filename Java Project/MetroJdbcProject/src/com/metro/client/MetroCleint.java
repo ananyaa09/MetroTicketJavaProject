352cf37567b5b27d0d2f892b905e121dbb6b40bb
Vijay Sahoo <76999159+Developer-Vijay@users.noreply.github.com>
2024-02-17 10:52:15 +0530

    Add files via upload

---
+++ Java Project/MetroJdbcProject/src/com/metro/client/MetroCleint.java
@@ -0,0 +1,8 @@
+package com.metro.client;
+
+public class MetroCleint {
+
+	public static void main(String args[]) {
+
+	}
+}
---
+++ Java Project/MetroJdbcProject/src/com/metro/exceptions/StationInvalidException.java
@@ -0,0 +1,12 @@
+package com.metro.exceptions;
+
+public class StationInvalidException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public StationInvalidException(String message) {
+		super(message);
+
+	}
+
+}
